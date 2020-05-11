d42d361d8b702cd33f36420b60088ffc868e19a5
Tutvydas Dulskis <tautvydas@dulskis.eu>
2020-05-11 21:27:58 +0300

    Po 7 paskaitos

---
+++ js/kampai.js
@@ -0,0 +1,7 @@
+let n1 = 3;
+let n2 = 4;
+
+s1 = (n1 - 2) * 180;
+s2 = (n2 - 2) * 180;
+
+document.getElementById('ats').innerHTML = s1 + s2;
---
+++ js/reziai.js
@@ -0,0 +1,8 @@
+let a = -5;
+let b = 0;
+let c = -4;
+let d = 4;
+
+if(a > b){
+    console.log('KLAIDA: A daugiau už B');
+}
